f8b80772f2a6ad3723c40e87cbf25a9589557e2a
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-21 11:26:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.download_data import download
+from src.preprocessing import download
 
 def main():
   
@@ -6,7 +7,10 @@ def main():
   # Downloads the Liar Dataset
   download()
 
+  # Process the data
+  X_train, y_train, X_test, y_test = process_data_pipeline('train.tsv', 'test.tsv', 'valid.tsv', truthiness_rank)
 
+  # Train our model
 
 if __name__ == '__main__':
   main()
