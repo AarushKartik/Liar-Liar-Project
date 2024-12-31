351ac1a6cf61b8cea44ba47c0bb64644fd340d04
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-12-30 20:35:20 -0500

    Update all_model_train.py

--- all_model_train.py
+++ all_model_train.py
@@ -1,7 +1,13 @@
 from src.download_data import download
-from src.preprocessing import process_data_pipeline
-from src.rnn_model import RNNClassifier
-from src.train_rnn import train
+
+# BERT
+from src.preprocessing_bert import process_data_pipeline_bert
+from src.bert_model import BERT
+from src.train_bert import train_bert
+
+# Roberta
+
+# BiLSTM
 
 def bilstm():
     print("Starting the pipeline...")
@@ -36,17 +42,17 @@ def bert():
 
     # Stage 2: Process the data
     print("Step 2: Preprocessing the data...")
-    X_train, y_train, X_test, y_test = process_data_pipeline('train.tsv', 'test.tsv', 'valid.tsv')
+    X_train, y_train, X_test, y_test = process_data_pipeline_bert('train.tsv', 'test.tsv', 'valid.tsv')
     print("Data preprocessing complete.\n")
 
     # Stage 3: Gather the model
-    print("Step 3: Building the RNN model...")
-    rnn = RNNClassifier(num_epochs=5, lstm_units=200, dropout_rate=0.2)
+    print("Step 3: Building the BERT model...")
+    rnn = BERT(num_epochs=5, lstm_units=200, dropout_rate=0.2)
     print("Model built successfully.\n")
 
     # Stage 4: Train the model
     print("Step 4: Training the model...")
-    train(rnn, X_train, X_test, y_train, y_test)
+    train_bert(rnn, X_train, X_test, y_train, y_test)
     print("Model training complete.\n")
 
 def roberta():
