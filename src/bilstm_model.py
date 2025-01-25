5872e33c6e8489d29481ad00a718a85185f3d079
AarushKartik <aarushkrtk@gmail.com>
2025-01-25 17:57:54 -0500

    Update bilstm_model.py

--- src/bilstm_model.py
+++ src/bilstm_model.py
@@ -10,7 +10,8 @@ import os
 class BiLSTMClassifier:
     def __init__(self, 
                  num_classes=6, 
-                 num_epochs=3, 
+                 num_epochs=3,
+                 lstm_units=200
                  dropout_rate=0.1, 
                  learning_rate=2e-5,
                  max_len=128,  # Max sequence length for padding/truncating input sequences
