54a051040a5e310d70f298ba55d28275cc3bfa44
AarushKartik <aarushkrtk@gmail.com>
2025-02-15 12:14:24 -0500

    Update roberta_model.py

--- src/roberta_model.py
+++ src/roberta_model.py
@@ -5,7 +5,7 @@ import os
 import shutil
 
 class RoBERTaClassifier:
-    def __init__(self, num_classes=6, num_epochs=3, dropout_rate=0.2, learning_rate=2e-5):
+    def __init__(self, num_classes=6, num_epochs=3, dropout_rate=0.1, learning_rate=3e-4):
         # Ensure the model uses GPU if available
         self.set_gpu_configuration()
         
