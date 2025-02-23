7c6e9365e755fe832d001bbf9ed9b3d5777f059a
AarushKartik <aarushkrtk@gmail.com>
2025-02-22 22:16:00 -0500

    Update bilstm_model.py

--- src/bilstm_model.py
+++ src/bilstm_model.py
@@ -100,6 +100,8 @@ class BiLSTMClassifier:
         return model
 
     def extract_features(self, texts, save_path=None, split_name=None):
+        if isinstance(texts, tuple):
+            texts = texts[0]
         """
         Converts pre-tokenized sequences (lists of string IDs) to integer arrays.
         :param texts: Input texts or tokenized sequences.
