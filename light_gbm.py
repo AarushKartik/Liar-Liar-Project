3da1651819ad6816b12d79c085c6e0c5d8404b1a
AarushKartik <aarushkrtk@gmail.com>
2025-03-01 11:38:01 -0500

    Update light_gbm.py

--- light_gbm.py
+++ light_gbm.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the LIAR dataset (assumes it is in TSV format)
-df = pd.read_tsv("train_file.tsv", sep="\t", header=None)
+df = pd.read_csv("train_file.tsv", sep="\t", header=None)
 
 # Assign column names based on LIAR dataset
 df.columns = ["statement", "label", "subject", "speaker", "speaker_affiliation", 
